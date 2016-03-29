bc599806a2d8676a56cc7d235bf90aeb71b6e5ee
Gauthier Flichy <ga2015@flichy.biz>
2016-03-29 20:09:06 +0200

    Article avec tag

--- src/AppBundle/Controller/Article/ArticleController.php
+++ src/AppBundle/Controller/Article/ArticleController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller\Article;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,12 +22,14 @@ class ArticleController extends Controller
     {
         return new Response('List of Article');
     }
+
     /**
-     * @Route("/show/{id}")
+     * @Route("/show/{id}", requirements={"id" = "\d+"})
      */
-    public function showAction($id)
+    public function showAction($id, Request $request)
     {
-        return new Response("Affiche l'article avec l'ID ".$id);
+       $tag = $request->query->get('tag');
+        return new Response("Affiche l'article avec l'ID ".$id. " avec le tag ".$tag);
     }
 
 
