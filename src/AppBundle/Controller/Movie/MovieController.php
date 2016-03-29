190b6b10c162e9139d7efb31740ed938552e58ef
Gauthier Flichy <ga2015@flichy.biz>
2016-03-29 20:32:08 +0200

    Ajout des movies

--- src/AppBundle/Controller/Movie/MovieController.php
+++ src/AppBundle/Controller/Movie/MovieController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace AppBundle\Controller\Article;
+namespace AppBundle\Controller\Movie;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController extends Controller
+class MovieController extends Controller
 {
     /**
      * @Route("/list")
@@ -22,9 +22,8 @@ class ArticleController extends Controller
      */
     public function showAction($id, Request $request)
     {
-       $tag = $request->query->get('tag');
-        return new Response("Affiche l'article avec l'ID ".$id. " avec le tag ".$tag);
+        $tag = $request->query->get('tag');
+        return new Response("Affiche le film avec l'ID ".$id. " avec le tag ".$tag);
+
     }
-
-
 }
