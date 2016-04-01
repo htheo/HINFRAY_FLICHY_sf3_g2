48821793652975d15922994236317b1c7e161a5e
htheo <hinfraytheo@gmail.com>
2016-04-01 15:13:26 +0200

    poisson d'avril

--- src/AppBundle/Controller/Article/ArticleController.php
+++ src/AppBundle/Controller/Article/ArticleController.php
@@ -55,6 +55,29 @@ class ArticleController extends Controller
             'articleName' => $articleName,
         ]);
     }
+    /**
+     * @Route("/author" , name="article_author"))
+     */
+    public function authorAction(Request $request)
+    {
+        $author = $request->query->get('author');
 
+        $em = $this->getDoctrine()->getManager();
+        $articleRepository = $em->getRepository('AppBundle:Article\Article');
+
+        $articles= $articleRepository->findBy([
+            'author' => $author,
+        ]);
+        return $this->render('AppBundle:Article:index.html.twig',[
+            'articles' => $articles,
+        ]);
+    }
+    /**
+     * @Route("/new")
+     */
+    public function newAction()
+    {
+
+    }
 
 }
